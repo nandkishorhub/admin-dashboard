51ddd17b44d25d605d33e52965841ef98dc0bb3a
Nandkishor <nandkishorcdacacts@gmail.com>
2022-07-24 10:58:23 +0530

    uploading project files first time

---
+++ index.js
@@ -0,0 +1,52 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
+
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
+
+ 
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme-variables");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+});
+
+// Fill orders in table
+
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${
+      order.shipping === "Declined"
+        ? "danger"
+        : order.shipping === "Pending"
+        ? "warning"
+        : "primary"
+    }">${order.shipping}</td>
+    <td class="primary">Details</td>
+    `;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
+
+// function handleTabletChange(e) {
+//   // Check if the media query is true
+//   if (e.matches) {
+//     sideMenu.style.display = "block";
+//   }
+// }
+
+// const mediaQuery = window.matchMedia('(min-width: 768px)')
+
+// // Register event listener
+// mediaQuery.addListener(handleTabletChange)
---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Sony 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Xperia 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "kodek 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Vivo 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "canon 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Nikon 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Delivered",
+  },
+  {
+    productName: "LG 3.1 Camera",
+    productNumber: "894",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+];
